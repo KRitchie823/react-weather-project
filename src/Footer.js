63b41efc2de81feafb5ec971da74104bb23dd30a
KRitchie823 <158782034+KRitchie823@users.noreply.github.com>
2024-08-16 14:47:21 -0400

    Added footer section with GitHub URL

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      This project was coded by Kim Ritchie and is {""}
+      <a
+        href="https://github.com/KRitchie823/react-weather-project"
+        target="_blank"
+      >
+        open-sourced on GitHub
+      </a>
+    </p>
+  );
+}
